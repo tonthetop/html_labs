fd15a5e8100186754a476f041261481e0eadc595
TontheTop <tuanak691@gmail.com>
2022-03-10 16:13:57 +0700

    lab1 - chap1

---
+++ Chapter11/lab1/scripts/scripts.js
@@ -0,0 +1,8 @@
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
